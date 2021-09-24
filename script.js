c40ad540beee2d6d232a4ea928ee8260fb6486d6
Amit Morade <65406995+Amit-Morade@users.noreply.github.com>
2021-09-24 08:50:17 +0530

    all files modified

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const input1 = document.querySelector("#color1");
 const input2 = document.querySelector("#color2");
 const msg = document.querySelector("#msg-window");
 const body = document.querySelector("body");
+let defaultoption = "to right";
+
+const select = document.querySelector("select");
+
+select.addEventListener("input", () => {
+	defaultoption = select.value;
+	changeColor();
+})
 
 const changeColor = () => {
-	body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`;
-	msg.textContent = `${input1.value} ${input2.value}`;
+	body.style.background = `linear-gradient(${defaultoption}, ${input1.value}, ${input2.value})`;
+	msg.textContent = `background : linear-gradient(${defaultoption}, ${input1.value}, ${input2.value})`;
 }
 
 
